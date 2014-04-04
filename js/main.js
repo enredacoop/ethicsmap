fcc916955bb09248152d6a45365ec44724ae167f
edipotrebol <edipotrebol@gmail.com>
2014-04-04 20:27:43 +0200

    Adding some changes to the map

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var map = L.map('map').
-    setView([37.39103, -5.99579], 15);
+    setView([37.37, -5.93], 13);
 
 // OFFICIAL http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+// OFFICIAL more info: http://{s}.tile.osm.org/{z}/{x}/{y}.png
 // BASIC http://{s}.acetate.geoiq.com/tiles/acetate/{z}/{x}/{y}.png
 // GEOGRAPHIC: http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png
+// http://{s}.tile.cloudmade.com/9067860284bc491e92d2342cc51d47d9/998/256/{z}/{x}/{y}.png
 
 L.tileLayer('http://{s}.tile.cloudmade.com/9067860284bc491e92d2342cc51d47d9/998/256/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href=”http://openstreetmap.org”>OpenStreetMap</a> contributors, <a href=”http://creativecommons.org/licenses/by-sa/2.0/”>CC-BY-SA</a>, Imagery © <a href=”http://cloudmade.com”>CloudMade</a>',
@@ -14,8 +16,7 @@ var geojsonMarkerOptions = {
     radius: 10,
     weight: 1,
     opacity: 1,
-    fillOpacity: 0.8,
-    color: "#111", fillColor: "#ffaa00"
+    fillOpacity: 0.8
 };
 
 L.geoJson(sevilla, {
@@ -23,22 +24,29 @@ L.geoJson(sevilla, {
         return L.circleMarker(latlng, geojsonMarkerOptions);
     },
     // style: function(feature) {
-    //     switch (feature.properties.category) {
+    //     console.log(feature.properties.cat);
+    //     switch (feature.properties.cat) {
     //         case 'good': return {color: "#111", fillColor: "#ffaa00"};
     //         case 'bad':   return {color: "#111", fillColor: "#ccc"};
+    //         default: return {color: "#111", fillColor: "#F00"};
     //     }
     // },
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup(feature.properties.name);
+    },
     // filter: function(feature, layer) {
-    //     return feature.properties.category == $('#select-category').val();
+    //     return feature.properties.cat == $('#select-category').val();
     // }
 }).addTo(map);
 
-var marker = L.marker([37.39103, -5.99579],{name: "Demo", draggable: true}).addTo(map);
+$('#map g').click(function(d) {
+    //
+});
 
-marker.on('click', function(d) { $('#content-box h3').text(d.latlng.toString()); });
 
-function onEachFeature(feature, layer) {
-    console.log(sevilla);
-    layer.bindPopup("<b>"+feature.properties.title +
-                    "</b><br>"+feature.properties.placename);
-}
+// if('geolocation' in navigator){
+//    navigator.geolocation.getCurrentPosition(success);
+// }
+// function success(pos){
+//    map.setView([pos.coords.latitude, pos.coords.longitude],13);
+// }
